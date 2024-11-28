ea0bec4644bf6cf418d72fc41398a6e85634f454
Babygrf <babytarget9@gmail.com>
2024-11-28 17:52:57 +0200

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=zccGgTwZ#dU79LIuB3-fbZm8f_hSXq7Pj7C0mYXO-hSKIIyuqxXc",
 };
